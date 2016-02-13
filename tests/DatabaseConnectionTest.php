1071d771db507857c1fc750c9dee9c1fb1a4dbeb
John Kariuki <john.kariuki@andela.com>
2016-02-13 22:05:01 +0300

    [Feature] [Tests] Assert Connection is created and destroyed

---
+++ tests/DatabaseConnectionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Potato\Tests;
+
+use Potato\Database\DatabaseConnection;
+use PHPUnit_Framework_Testcase;
+use PDO;
+
+class DatabaseConnectionTest extends PHPUnit_Framework_Testcase
+{
+    /**
+     * testDatabaseConnection
+     *
+     * Test connect method creates a database connection
+     *
+     * Test close method closes a datanase connection
+     *
+     * @return void
+     */
+    public function testDatabaseConnection()
+    {
+        $connection = DatabaseConnection::connect();
+
+        $this->assertTrue(is_object($connection));
+        $this->assertTrue($connection instanceof PDO);
+
+        $connection = DatabaseConnection::close();
+
+        $this->assertNull($connection);
+    }
+}
